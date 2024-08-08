9c4f3cf41b9946823c58ccf8e8389a07ffee1afd
MariaGins <m.gins@mail.ru>
2024-08-08 10:21:32 +0300

    wrote method getMinimalBrokenFloor() with using binary search

---
+++ src/test/java/telran/BrokenFloorTest.java
@@ -0,0 +1,36 @@
+package telran;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import telran.range.BallBrokenFloor;
+
+public class BrokenFloorTest {
+
+
+    private int getMinimalBrokenFloor(BallBrokenFloor bbf) {
+        int low_floor = 0;
+        int high_floor = Integer.MAX_VALUE;
+        int result = -1;
+        while (low_floor <= high_floor) {
+            int middle = (low_floor + high_floor) / 2;
+            try {
+                bbf.checkFloor(middle);
+                low_floor = middle + 1;
+            } catch (Exception e) {
+                result = middle;
+                high_floor = middle - 1;
+            }
+        }
+        return result;
+    }
+@Test
+void getMinimalBrokenFloorTest() {
+    int [] floors = {200, 17, 1001, 2000};
+    for(int i = 0; i < floors.length; i++) {
+        BallBrokenFloor bbf = new BallBrokenFloor(floors[i]);
+        assertEquals(bbf.getMinBrokenFloor(), getMinimalBrokenFloor(bbf));
+    }
+}
+}
