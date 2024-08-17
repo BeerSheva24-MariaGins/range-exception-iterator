e07dd4817088d3db3f3b7cf2458e718358593ea0
MariaGins <m.gins@mail.ru>
2024-08-17 20:03:01 +0300

    Added new functional for range processing

--- src/main/java/telran/range/Range.java
+++ src/main/java/telran/range/Range.java
@@ -1,15 +1,21 @@
 package telran.range;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.function.Predicate;
+
 import telran.range.exceptions.OutOfRangeMaxValueException;
 import telran.range.exceptions.OutOfRangeMinValueException;
 
-public class Range {
+@SuppressWarnings("unused")
+public class Range implements Iterable<Integer>{
     private static final String ERROR_MESSAGE = "max less or equal min";
     private int min;
     private int max;
+    private Predicate<Integer> predicate;
     private Range(int min, int max) {
         this.min = min;
-        this.max = max;
+        this.max = max; 
     }
     public static Range getRange(int min, int max) {
         if (max <= min) {
@@ -17,12 +23,40 @@ public class Range {
         }
         return new Range(min, max);
     }
+    void setPredicate(Predicate<Integer> predicate) {
+        this.predicate = predicate;
+    }
     public void checkNumber(int number) throws OutOfRangeMaxValueException, OutOfRangeMinValueException {
-        if (number > max) {
+        if(number > max) {
             throw new OutOfRangeMaxValueException(max, number);
         }
         if (number < min) {
             throw new OutOfRangeMinValueException(min, number);
+        }
+    }
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RangeIterator();
+    }
+    private class RangeIterator implements Iterator<Integer> {
+        int current;
+        @Override
+        public boolean hasNext() {
+            while (current <= max) {
+                if (predicate == null || predicate.test(current)) {
+                    return true;
+                }
+                current++;
+            }
+            return false;
+        }
+
+        @Override
+        public Integer next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return current++;
         }
     }
 }
